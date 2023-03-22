23c7758094f4a62e9c83b76ad2555b1e22203f20
Safa0202 <126269145+Safa0202@users.noreply.github.com>
2023-03-22 11:46:48 +0100

    Update TreeBeforeDB.js

--- TreeBeforeDB.js
+++ TreeBeforeDB.js
@@ -3,7 +3,6 @@ var getScriptPromisify = (src) => {
         $.getScript(src, resolve)
     })
 }
-
 (function () {
     const prepared = document.createElement('template')
     prepared.innerHTML = `
@@ -15,49 +14,41 @@ var getScriptPromisify = (src) => {
     class SamplePrepared extends HTMLElement {
         constructor() {
             super()
-
             this._shadowRoot = this.attachShadow({
                 mode: 'open'
             })
             this._shadowRoot.appendChild(prepared.content.cloneNode(true))
-
             this._root = this._shadowRoot.getElementById('root')
-
             this._props = {}
-
             this.render()
         }
-
         onCustomWidgetResize(width, height) {
             this.render()
         }
-        
 
         async render() {
-            await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js');
-
-            const chart = echarts.init(this._root)
+			await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
+			 const chart = echarts.init(this._root)
             chart.showLoading();
-            $.get('https://echarts.apache.org/examples/data/asset/data/flare.json', function (data) {
-            
-            data = JSON.parse(data);
-            chart.hideLoading();
+			$.get('https://raw.githubusercontent.com/apache/echarts-examples/gh-pages/public/data/asset/data/flare.json', function (data) {
+				 data = JSON.parse(data);
+				  chart.hideLoading();
                 data.children.forEach(function (datum, index) {
                     index % 2 === 0 && (datum.collapsed = true);
                 });
-                const option = {
-                        tooltip: {
+				const option = {
+					 tooltip: {
                             trigger: 'item',
                             triggerOn: 'mousemove'
                         },
                         series: [
                             {
                                 type: 'tree',
-                                data: [data],
+								 data: [data],
                                 top: '1%',
                                 left: '7%',
                                 bottom: '1%',
-                                right: '20%',
+								 right: '20%',
                                 symbolSize: 7,
                                 label: {
                                     position: 'left',
@@ -80,10 +71,11 @@ var getScriptPromisify = (src) => {
                                 animationDurationUpdate: 750
                             }
                         ]
-                    }
+						  }
               chart.setOption(option)
-            });
-        }
+			    });
+				 }
     }
     customElements.define('com-sap-sample-echarts-prepared3', SamplePrepared)
-})
+	})()
+	
